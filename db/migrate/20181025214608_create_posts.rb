76a1017681558ec5f2b6f43ac5df99822609a124
Adhiv Dhar <adhivd@gmail.com>
2018-10-25 14:48:36 -0700

    Post rollback and scaffold

--- db/migrate/20181025214608_create_posts.rb
+++ db/migrate/20181025214608_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[5.2]
   def change
     create_table :posts do |t|
       t.references :user, foreign_key: true
-      t.text :description
+      t.text :caption
 
       t.timestamps
     end
