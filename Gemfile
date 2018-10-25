70116263106c8d8b8c18b16c431d607a5ca2ec5a
Adhiv Dhar <adhivd@gmail.com>
2018-10-25 14:36:45 -0700

    Faker set up in seeds.rb + Post model created

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 ruby '2.5.1'
 
 gem 'devise'
+gem 'faker'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.0'
 gem 'pg'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Create your own Fake Data! https://github.com/stympy/faker
+
+require 'faker'
+
+10.times do |index|
+  User.create!(first_name: Faker::Name.first_name,
+                    last_name: Faker::Name.last_name,
+                    bio: Faker::Twitter.status[:text],
+                    email: Faker::Internet.email,
+                    password: Faker::Internet.password
+                    )
+end
