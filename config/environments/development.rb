4feb9005c36b63ec3adf922b0ef8cc75a99d1b54
Adhiv Dhar <adhivd@gmail.com>
2018-10-25 11:12:29 -0700

    devise setup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,8 +54,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 1337 }
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
