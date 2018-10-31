8c7fd40d3552f517df873d8940d9b13857d6eaf5
Ethan Lee <hello@ethanl.ee>
2018-10-31 13:48:08 -0700

    Photos with file uploads

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -81,6 +81,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:user_id, :caption)
+      params.require(:post).permit(:user_id, :caption, :photo)
     end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :users_liked, through: :likes, source: :user
+  has_one_attached :photo
 end
